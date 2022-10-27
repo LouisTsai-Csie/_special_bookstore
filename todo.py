5aeee06860ae4c930af1265f2aae943bc13ddf39
LouisTsai-Csie <Q1030176@gmail.com>
2022-10-27 20:21:32 +0800

    comment

---
+++ todo.py
@@ -0,0 +1,18 @@
+def app():
+    bookstoreList = getAllBookstore()
+
+    countyOption = getCountyOption(bookstoreList)
+
+    st.header('特色書店地圖')
+    st.metric('Total bookstore', len(bookstoreList))
+    county = st.selectbox('請選擇縣市', countyOption)
+    districtOption = getDistrictOption(bookstoreList, county)
+    district = st.multiselect('請選擇區域', districtOption)
+
+    specificBookstore = getSpecificBookstore(bookstoreList, county, district)
+    num = len(specificBookstore)
+    st.write(f'總共有{num}項結果', num)
+		
+		# 幫 specificBookstore 進行排序
+		# hint: 使用 lambda 和 sort
+    bookstoreInfo = getBookstoreInfo(specificBookstore)
