868c8f2d1ed3c2b5267a86410f51aa75d059ee99
LouisTsai-Csie <Q1030176@gmail.com>
2022-10-27 16:53:00 +0800

    comment

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import requests
+
+def getAllBookstore() ->list:
+    url = "https://cloud.culture.tw/frontsite/trans/emapOpenDataAction.do?method=exportEmapJson&typeId=M"
+    headers = {"accept": "application/json"}
+    response = requests.get(url, headers=headers)
+    res = response.json()
+    return res
+
+def getCountyOption(items) ->list:
+    optionList = []
+    for item in items:
+        name = item['cityName'][0:3]
+        if name not in optionList:
+            optionList.append(name)
+    return optionList
+
+def app():
+    bookstoreList = getAllBookstore()
+    countyOption = getCountyOption(bookstoreList)
+    st.header('特色書店地圖')
+    st.metric('Total bookstore', len(bookstoreList))
+    county = st.selectbox('請選擇縣市', countyOption) 
+    district = st.multiselect('請選擇區域', ['a', 'b', 'c', 'd'])
+
+if __name__ == '__main__':
+    app()
