4782224415d531c6c71c48ceab313771a50732cc
LouisTsai-Csie <Q1030176@gmail.com>
2022-10-27 16:55:52 +0800

    bug fixed

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ import streamlit as st
 import requests
 
 def getAllBookstore() ->list:
-    url = "https://cloud.culture.tw/frontsite/trans/emapOpenDataAction.do?method=exportEmapJson&typeId=M"
-    headers = {"accept": "application/json"}
-    response = requests.get(url, headers=headers)
-    res = response.json()
-    return res
+	url = '' # 在這裡輸入目標 url
+	headers = {"accept": "application/json"}
+	response = request.get(url, headers=headers)
+	# 將 response 轉換成 json 格式
+	# 回傳值
 
 def app():
-    bookstoreList = getAllBookstore()
+	# 呼叫 getAllBookstore 函式並將其賦值給變數 bookstoreList
     st.header('特色書店地圖')
-    st.metric('Total bookstore', len(bookstoreList))
-    county = st.selectbox('請選擇縣市', ['A', 'B', 'C'])
-    district = st.multiselect('請選擇區域', ['a', 'b', 'c', 'd'])
+	st.metric('Total bookstore', 118) # 將 118 替換成書店的數量
+	county = st.selectbox('請選擇縣市', ['A', 'B', 'C'])
+	district = st.multiselect('請選擇區域', ['a', 'b', 'c', 'd'])
 
 if __name__ == '__main__':
     app()
