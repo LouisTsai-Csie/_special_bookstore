04af05259fe8bca24f629cdd18fc4b2d09e52181
LouisTsai-Csie <Q1030176@gmail.com>
2022-10-27 16:54:40 +0800

    bug fixed

--- app.py
+++ app.py
@@ -9,20 +9,24 @@ def getAllBookstore() ->list:
     return res
 
 def getCountyOption(items) ->list:
-    optionList = []
+    # 創建一個空的 List 並命名為 optionList
     for item in items:
-        name = item['cityName'][0:3]
-        if name not in optionList:
-            optionList.append(name)
+        # 把 cityname 欄位中的縣市名稱擷取出來 並指定給變數 name
+				# hint: 想辦法處理 item['cityName'] 的內容
+
+        # 如果 name 不在 optionList 之中，便把它放入 optionList
+				# hint: 使用 if-else 來進行判斷 / 用 append 把東西放入 optionList
     return optionList
 
 def app():
     bookstoreList = getAllBookstore()
-    countyOption = getCountyOption(bookstoreList)
+
+	# 呼叫 getCountyOption 並將回傳值賦值給變數 countyOption
+    
     st.header('特色書店地圖')
-    st.metric('Total bookstore', len(bookstoreList))
-    county = st.selectbox('請選擇縣市', countyOption) 
-    district = st.multiselect('請選擇區域', ['a', 'b', 'c', 'd'])
+	st.metric('Total bookstore', len(bookstoreList))
+	county = st.selectbox('請選擇縣市', ['A', 'B', 'C']) # 將['A', 'B', 'C']替換成縣市選項 
+	district = st.multiselect('請選擇區域', ['a', 'b', 'c', 'd'])
 
 if __name__ == '__main__':
     app()
